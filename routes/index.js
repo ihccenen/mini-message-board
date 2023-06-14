05e055642e36e2ab63da371f9f621f5c96582bd3
ihccenen <wirkz01@gmail.com>
2023-06-14 00:23:36 -0300

    Added messages, form and moment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 const express = require('express');
+const moment = require('moment');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Homepage' });
+  res.render('index', { title: 'Mini Messageboard', messages, moment });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', (req, res) => {
+  const { messageUser, messageText } = req.body;
+
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+
+  res.redirect('/');
 });
 
 module.exports = router;
